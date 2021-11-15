5a1e9a21bd3393c4258ab477a98b68e51d1fc5c3
Tolganay Muntinova <78073956+muntinova@users.noreply.github.com>
2021-11-15 12:24:06 -0600

    Revert "table list"

--- CourseModule/src/app/table-list/table-list.service.ts
+++ CourseModule/src/app/table-list/table-list.service.ts
@@ -1,29 +1,26 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableListService {
+  url: string ="http://localhost:4000/course/"
 
-  url: string = "http://localhost:4000/course/";
+  constructor(private http:HttpClient) {
 
-  constructor(private http: HttpClient) { }
+   }
 
+   getData(){
+     let header=new Headers();
+     header.append('Content-Type','application/json');
+     return this.http.get(this.url).pipe(map((res:any)=>res));
+   }
 
-  getData() {
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    return this.http.get(this.url)
-      .pipe(map((res: any) => res));
-  }
-
-
-  deleteData(id: string) {
-    let myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    return this.http.delete(this.url + id)
-      .pipe(map((res: any) => res));
-  }
+   deleteData(id:any){
+     let header=new Headers();
+     header.append('Content-Type','application/json');
+     return this.http.delete(this.url+id).pipe(map((res:any)=>res));
+   }
 }
